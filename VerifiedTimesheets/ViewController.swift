38af6e2a683382b5e162df77e7708020aeeb0782
Alex Hokanson <alex.hokanson@inin.com>
2015-01-03 13:09:48 -0500

    Initial Commit

---
+++ VerifiedTimesheets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VerifiedTimesheets
+//
+//  Created by Alex.Hokanson on 1/3/15.
+//  Copyright (c) 2015 Alex Hokanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
